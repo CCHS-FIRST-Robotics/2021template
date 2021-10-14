2d724030d295f04e687b50b0e1da80eca3f22074
ctay22 <ctay22@concordcarlisle.org>
2021-10-14 15:05:42 -0400

    format

--- src/main/java/frc/robot/sensors/DriveEncoderSensor.java
+++ src/main/java/frc/robot/sensors/DriveEncoderSensor.java
@@ -6,48 +6,52 @@ import frc.robot.HardwareObjects;
 import com.ctre.phoenix.motorcontrol.can.*;
 import java.lang.Math;
 
-public class DriveEncoderSensor extends BaseSensor{
+public class DriveEncoderSensor extends BaseSensor {
     double VARIANCE = 0.01;
-    public DriveEncoderSensor(double sync_time){
-        this.LAG_TIME = 0.0; //No Lag
+
+    public DriveEncoderSensor(double sync_time) {
+        this.LAG_TIME = 0.0; // No Lag
         this.SYNC_TIME = sync_time;
         this.last_updated = sync_time;
     }
-    public boolean shouldUse(){
+
+    public boolean shouldUse() {
         return true;
     }
-    public boolean commonSense(double rpm){
-        if (rpm>300){
+
+    public boolean commonSense(double rpm) {
+        if (rpm > 300) {
             return false;
         }
-        if (rpm<-300){
+        if (rpm < -300) {
             return false;
         }
         return true;
     }
-    public void processValue(MainState state){
+
+    public void processValue(MainState state) {
         double l_raw = HardwareObjects.LEFT_MOTOR.getSelectedSensorVelocity(1);
         double r_raw = HardwareObjects.RIGHT_MOTOR.getSelectedSensorVelocity(1);
-        //Convert to rads per sec
-        double l_radss = l_raw*2*Math.PI/60;
-        double r_radss = r_raw*2*Math.PI/60;
-        
+        // Convert to rads per sec
+        double l_radss = l_raw * 2 * Math.PI / 60;
+        double r_radss = r_raw * 2 * Math.PI / 60;
+
         double l_pred_radss = state.getLWheelVelVal();
         double r_pred_radss = state.getRWheelVelVal();
 
         double l_pred_var = state.getLWheelVelVar();
         double r_pred_var = state.getRWheelVelVar();
 
-        if (commonSense(l_radss)){
+        if (commonSense(l_radss)) {
             double[] l_new = state.kalmanUpdate(l_pred_radss, l_pred_var, l_radss, VARIANCE);
             state.setLWheelVel(l_new[0], l_new[1]);
         }
-        if (commonSense(r_radss)){
+        if (commonSense(r_radss)) {
             double[] r_new = state.kalmanUpdate(r_pred_radss, r_pred_var, r_radss, VARIANCE);
             state.setRWheelVel(r_new[0], r_new[1]);
         }
 
-        //Logging
+        // Logging
         System.out.println("Left Encoder Velocity: " + String.valueOf(l_radss));
         System.out.println("Right Encoder Velocity: " + String.valueOf(r_radss));
     }
--- src/main/java/frc/robot/state/Kinematics.java
+++ src/main/java/frc/robot/state/Kinematics.java
@@ -1,7 +1,9 @@
 package frc.robot.state;
+
 import frc.robot.Constants;
 import frc.robot.helper.SimpleMat;
 import java.lang.Math;
+
 public class Kinematics {
 
     public double mix = Constants.INIT_MIX;
@@ -12,8 +14,8 @@ public class Kinematics {
 
     public Values Val = new Values();
     public Variances Var = new Variances();
-    
-    public Kinematics(){
+
+    public Kinematics() {
         this.mix = Constants.INIT_MIX;
         this.delta_weight = Constants.INIT_DW;
         this.friction = Constants.INIT_FRICTION;
@@ -21,10 +23,11 @@ public class Kinematics {
         this.r_whl_trac = Constants.INIT_R_WHL_TRAC;
     }
 
-    public void predict(double dt){
+    public void predict(double dt) {
         this.Val.predict(dt);
-        this.Var.predict(dt);   
+        this.Var.predict(dt);
     }
+
     class Values {
         public double[] pos = Constants.INIT_POS;
         public double[] vel = Constants.INIT_VEL;
@@ -37,89 +40,77 @@ public class Kinematics {
         public double l_whl_vel = Constants.INIT_L_WHL_VEL;
         public double r_whl_vel = Constants.INIT_R_WHL_VEL;
 
-
-
-        public Values(){
+        public Values() {
             this.pos = Constants.INIT_POS;
             this.vel = Constants.INIT_VEL;
             this.acc = Constants.INIT_ACC;
-    
+
             this.heading = Constants.INIT_HEADING;
             this.ang_vel = Constants.INIT_ANG_VEL;
-    
+
             this.l_whl_vel = Constants.INIT_L_WHL_VEL;
             this.r_whl_vel = Constants.INIT_R_WHL_VEL;
-    
+
         }
 
-        public void predict(double dt){
-            //==============
-            //WHEEL ODOMOTRY CALCULATIONS
-            //==============
-            double l = this.l_whl_vel*Constants.WHEEL_RADIUS * l_whl_trac;
-            double r = this.r_whl_vel*Constants.WHEEL_RADIUS * l_whl_trac;
+        public void predict(double dt) {
+            // ==============
+            // WHEEL ODOMOTRY CALCULATIONS
+            // ==============
+            double l = this.l_whl_vel * Constants.WHEEL_RADIUS * l_whl_trac;
+            double r = this.r_whl_vel * Constants.WHEEL_RADIUS * l_whl_trac;
 
-            double arc_angle = dt*(r-l)/Constants.ROBOT_WIDTH;
+            double arc_angle = dt * (r - l) / Constants.ROBOT_WIDTH;
 
-            double[] o_local_delta = {0,0};
-            if (r==l){
+            double[] o_local_delta = { 0, 0 };
+            if (r == l) {
                 o_local_delta[0] = 0;
                 o_local_delta[1] = r;
-            }
-            else{
-                double travel_mag = Constants.ROBOT_WIDTH*(l+r)/(2*(r-l));
+            } else {
+                double travel_mag = Constants.ROBOT_WIDTH * (l + r) / (2 * (r - l));
                 o_local_delta[0] = travel_mag * (Math.cos(arc_angle) - 1);
-                o_local_delta[1] = travel_mag * (Math.sin(arc_angle)    );
+                o_local_delta[1] = travel_mag * (Math.sin(arc_angle));
             }
             double[] o_delta = SimpleMat.rot2d(o_local_delta, this.heading);
 
-            double[] temp_vel = {this.vel[0], this.vel[1]};
-            //FRICTION ACCEL
+            double[] temp_vel = { this.vel[0], this.vel[1] };
+            // FRICTION ACCEL
             double[] vel_unit = SimpleMat.unitVec(this.vel);
-            double[] friction_a = {friction * Constants.GRAV_ACC * vel_unit[0], friction * Constants.GRAV_ACC * vel_unit[1]};
-            //POS
-            this.pos[0] = this.pos[0] 
-                + mix * (this.vel[0]*dt + 0.5 * (this.acc[0] + friction_a[0]) * dt * dt) 
-                + (1 - mix) * o_delta[0];
-            this.pos[1] = this.pos[1] 
-                + mix * (this.vel[1]*dt + 0.5 * (this.acc[1] + friction_a[1]) * dt * dt)
-                + (1 - mix) * o_delta[1];
-            //VEL
-            this.vel[0] = this.vel[0] 
-                + mix * ((this.acc[0] - friction_a[0]) * dt) 
-                + (1 - mix) * (o_delta[0]/dt);
-            this.vel[1] = this.vel[1] 
-                + mix * ((this.acc[1] - friction_a[1]) * dt) 
-                + (1 - mix) * (o_delta[1]/dt);
-            //ACCEL
-            this.acc[0] = delta_weight * this.acc[0] 
-                + (1 - delta_weight) * (this.vel[0] - temp_vel[0])/dt;
-            this.acc[1] = delta_weight * this.acc[1] 
-                + (1 - delta_weight) * (this.vel[1] - temp_vel[1])/dt;
-            //HEADING
-            double m_o_i = Constants.ROBOT_WIDTH*Constants.ROBOT_WIDTH*Constants.ROBOT_MASS*0.125;
-            double ang_fric = (this.ang_acc/Math.abs(this.ang_acc))*Constants.ROBOT_WIDTH*Constants.GRAV_ACC*friction/(2*m_o_i);
-            
+            double[] friction_a = { friction * Constants.GRAV_ACC * vel_unit[0],
+                    friction * Constants.GRAV_ACC * vel_unit[1] };
+            // POS
+            this.pos[0] = this.pos[0] + mix * (this.vel[0] * dt + 0.5 * (this.acc[0] + friction_a[0]) * dt * dt)
+                    + (1 - mix) * o_delta[0];
+            this.pos[1] = this.pos[1] + mix * (this.vel[1] * dt + 0.5 * (this.acc[1] + friction_a[1]) * dt * dt)
+                    + (1 - mix) * o_delta[1];
+            // VEL
+            this.vel[0] = this.vel[0] + mix * ((this.acc[0] - friction_a[0]) * dt) + (1 - mix) * (o_delta[0] / dt);
+            this.vel[1] = this.vel[1] + mix * ((this.acc[1] - friction_a[1]) * dt) + (1 - mix) * (o_delta[1] / dt);
+            // ACCEL
+            this.acc[0] = delta_weight * this.acc[0] + (1 - delta_weight) * (this.vel[0] - temp_vel[0]) / dt;
+            this.acc[1] = delta_weight * this.acc[1] + (1 - delta_weight) * (this.vel[1] - temp_vel[1]) / dt;
+            // HEADING
+            double m_o_i = Constants.ROBOT_WIDTH * Constants.ROBOT_WIDTH * Constants.ROBOT_MASS * 0.125;
+            double ang_fric = (this.ang_acc / Math.abs(this.ang_acc)) * Constants.ROBOT_WIDTH * Constants.GRAV_ACC
+                    * friction / (2 * m_o_i);
 
-            this.heading = this.heading +
-             mix * (this.ang_vel * dt + 0.5*(this.ang_acc + ang_fric)*dt*dt) + 
-             (1 - mix) * arc_angle;
-            this.heading = this.heading % (2*Math.PI);
-            //ANG VEL
-            if (Math.abs(this.ang_acc)<Math.abs(ang_fric)){
-                if (Math.abs(this.ang_acc + ang_fric)*dt > Math.abs(this.ang_vel)){
+            this.heading = this.heading + mix * (this.ang_vel * dt + 0.5 * (this.ang_acc + ang_fric) * dt * dt)
+                    + (1 - mix) * arc_angle;
+            this.heading = this.heading % (2 * Math.PI);
+            // ANG VEL
+            if (Math.abs(this.ang_acc) < Math.abs(ang_fric)) {
+                if (Math.abs(this.ang_acc + ang_fric) * dt > Math.abs(this.ang_vel)) {
                     this.ang_vel = 0;
+                } else {
+                    this.ang_vel = this.ang_vel + (this.ang_acc + ang_fric) * dt;
                 }
-                else{
-                    this.ang_vel = this.ang_vel + (this.ang_acc + ang_fric)*dt;
-                }
+            } else {
+                this.ang_vel = this.ang_vel + (this.ang_acc + ang_fric) * dt;
             }
-            else{
-                this.ang_vel = this.ang_vel + (this.ang_acc + ang_fric)*dt;
-            }
-            
+
         }
     }
+
     class Variances {
         public double pos = Constants.INIT_VARIANCE;
         public double vel = Constants.INIT_VARIANCE;
@@ -132,31 +123,27 @@ public class Kinematics {
         public double l_whl_vel = Constants.INIT_VARIANCE;
         public double r_whl_vel = Constants.INIT_VARIANCE;
 
-
-
-        public Variances(){
+        public Variances() {
             this.pos = Constants.INIT_VARIANCE;
             this.vel = Constants.INIT_VARIANCE;
             this.acc = Constants.INIT_VARIANCE;
-    
+
             this.heading = Constants.INIT_VARIANCE;
             this.ang_vel = Constants.INIT_VARIANCE;
-    
+
             this.l_whl_vel = Constants.INIT_VARIANCE;
             this.r_whl_vel = Constants.INIT_VARIANCE;
-    
+
         }
-        public void predict(double dt){
-            double l = this.l_whl_vel*Constants.WHEEL_RADIUS * l_whl_trac;
-            double r = this.r_whl_vel*Constants.WHEEL_RADIUS * r_whl_trac;
-            double arc_angle = dt*(r-l)/Constants.ROBOT_WIDTH;
-            this.pos = this.pos
-                + mix * (this.vel * dt + 0.5 * this.acc * dt * dt)
-                + (1-mix) * (l + r);
+
+        public void predict(double dt) {
+            double l = this.l_whl_vel * Constants.WHEEL_RADIUS * l_whl_trac;
+            double r = this.r_whl_vel * Constants.WHEEL_RADIUS * r_whl_trac;
+            double arc_angle = dt * (r - l) / Constants.ROBOT_WIDTH;
+            this.pos = this.pos + mix * (this.vel * dt + 0.5 * this.acc * dt * dt) + (1 - mix) * (l + r);
             this.vel = this.vel + this.acc * dt;
-            this.heading = this.heading 
-                + mix * (this.ang_vel*dt + 0.5*this.ang_acc*dt*dt)
-                + (1-mix) * arc_angle;
+            this.heading = this.heading + mix * (this.ang_vel * dt + 0.5 * this.ang_acc * dt * dt)
+                    + (1 - mix) * arc_angle;
             this.ang_vel = this.ang_vel + this.ang_acc * dt;
         }
     }
