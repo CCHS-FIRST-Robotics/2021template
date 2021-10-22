814a693dcfabba1c6756f80e10d53a2bdda916c7
ctay22 <ctay22@concordcarlisle.org>
2021-10-22 10:13:00 -0400

    backup wheel odo phys branch

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,31 +5,35 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
  */
 public final class Constants {
-    //==================
+    // ==================
     // HARDWARE PORTS
-    //==================
+    // ==================
     public static final int R_TALON_PORT = 6;
-    public static final int L_TALON_PORT  = 9;
+    public static final int L_TALON_PORT = 9;
 
     public static final int R_VICTOR_PORT = 1;
-    public static final int L_VICTOR_PORT  = 2;
+    public static final int L_VICTOR_PORT = 2;
+
+    public static final int IMU_PORT = 5;
 
     public static final int XBOX_PORT = 0;
 
-    //==================
-    //STATE INITIALIZATION
-    //==================
-    public static final double[] INIT_POS = {0,0};
-    public static final double[] INIT_VEL = {0,0};
-    public static final double[] INIT_ACC = {0,0};
+    // ==================
+    // STATE INITIALIZATION
+    // ==================
+    public static final double[] INIT_POS = { 0, 0 };
+    public static final double[] INIT_VEL = { 0, 0 };
+    public static final double[] INIT_ACC = { 0, 0 };
 
     public static final double INIT_HEADING = 0;
     public static final double INIT_ANG_VEL = 0;
@@ -48,37 +52,37 @@ public final class Constants {
     public static final double INIT_DW = 0.4;
 
     public static final double INIT_VARIANCE = 0.01;
-    //==================
-    //VARIANCE LINEAR APPROX
-    //==================
+    // ==================
+    // VARIANCE LINEAR APPROX
+    // ==================
     public static final double ANG_VEL_VARIANCE = 0.5;
     public static final double ACC_VARIANCE = 0.1;
 
-    //==================
-    //TIME CONSTANTS
-    //==================
+    // ==================
+    // TIME CONSTANTS
+    // ==================
     public static final double MAIN_DT = 0.02;
 
-    //==================
-    //MOTOR CONSTANTS
-    //==================
-    public static final double MOTOR_MAX_POWER = 674; //OUTPUT IN WATTS
+    // ==================
+    // MOTOR CONSTANTS
+    // ==================
+    public static final double MOTOR_MAX_POWER = 674; // OUTPUT IN WATTS
 
     public static final double MOTOR_MAX_TORQUE = 4.82;
 
     public static final double MOTOR_MAX_RPM = 666;
-    
+
     public static final double MOTOR_PROP_VAR = 0.1;
 
     public static final int TIMEOUT_MS = 50;
 
-    //==================
-    //CERTAIN PHYSICAL CONSTANTS
-    //==================
+    // ==================
+    // CERTAIN PHYSICAL CONSTANTS
+    // ==================
     public static final double ROBOT_WIDTH = 0.635;
     public static final double WHEEL_RADIUS = 0.09;
     public static final double ROBOT_MASS = 40;
-    
+
     public static final double GRAV_ACC = -9.81;
-    
+
 }
--- src/main/java/frc/robot/HardwareObjects.java
+++ src/main/java/frc/robot/HardwareObjects.java
@@ -1,13 +1,16 @@
 package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.can.*;
+import com.ctre.phoenix.sensors.*;
 
 public class HardwareObjects {
     public TalonSRX LEFT_MOTOR;
     public TalonSRX RIGHT_MOTOR;
+    public PigeonIMU IMU;
 
     public HardwareObjects() {
         LEFT_MOTOR = new WPI_TalonSRX(Constants.L_TALON_PORT);
         RIGHT_MOTOR = new WPI_TalonSRX(Constants.R_TALON_PORT);
+        IMU = new PigeonIMU(Constants.IMU_PORT);
     }
 }
