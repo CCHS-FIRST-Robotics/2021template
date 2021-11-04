e324f5e0277884f147a72738408fbe1df7f923f2
pereos <danieljiang2358@gmail.com>
2021-11-04 15:57:20 -0400

    ai folder name follows naming convention

---
+++ src/main/java/frc/robot/ai/finite_state/AutonomousTestDrive.java
@@ -0,0 +1,24 @@
+package frc.robot.ai.finite_state;
+
+import frc.robot.state.*;
+import frc.robot.commands.*;
+
+public class AutonomousTestDrive {
+    double start_time = 0;
+
+    public AutonomousTestDrive() {
+        this.start_time = (double) System.currentTimeMillis() / 1000;
+    }
+
+    public void initAuto() {
+        this.start_time = (double) System.currentTimeMillis() / 1000;
+    }
+
+    public Command getCommands(MainState state) {
+        if ((double) System.currentTimeMillis() / 1000 < this.start_time + 1) {
+            return CommandHelper.computeCommand(0.2, 0.2);
+        } else {
+            return CommandHelper.computeCommand(0, 0);
+        }
+    }
+}
